c9dea55aadbd75ff94fe37467dc1c1a182f98cfc
Aday <>
2023-05-09 21:10:09 +0600

    Refactoring authorization

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { validationResult } from "express-validator";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { registerValidation } from "../validation/auth.js";
-import UserModel from "../models/User.js";
+import checkAuth from "../utils/checkAuth.js";
+import * as UserControllers from "../controlles/UserControllers.js";
 
 const app = express();
 const port = 3000;
@@ -26,72 +24,9 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/auth.login", async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
-
-    if (!user) {
-      return res.status(400).json({
-        message: "Пользователь не найден",
-      });
-    }
-
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-
-    if (!isValidPass) {
-      return req.status(404).json({
-        message: "Неверный логин или пароль",
-      });
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.post("/auth/register", registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({ ...userData, token });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Failed to register!",
-    });
-  }
-});
+app.post("/auth/register", registerValidation, UserControllers.register);
+app.post("/auth/login", UserControllers.login);
+app.get("/auth/me", checkAuth, UserControllers.getMe);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
